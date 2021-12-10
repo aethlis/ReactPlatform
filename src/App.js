41c1e832c4f55b3d12c55fe7a88c7dcfda7ba621
aethlis <84690253+aethlis@users.noreply.github.com>
2021-12-10 12:31:53 +0900

    ada

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/:id"  element={<Detail/>}>
         </Route>
-        <Route exact path="/"  element={<Home/>}>
+        <Route path="/"  element={<Home/>}>
         </Route>
       </Routes>
     </Router>
